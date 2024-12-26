8990f0912b0e7e2347fbd2227379971129d1d435
artimusmaximus <arthurmodymaniv@gmail.com>
2024-12-26 09:50:37 -0800

    question4 part 1 complete

--- question4/q4.js
+++ question4/q4.js
@@ -66,15 +66,34 @@ class CrossWord {
         return this;
     }
     regex(str) {
-        const regex = /samxmas|xmasamx|xmas|samx/gi
+        // const regex = /samxmas|xmasamx|xmas|samx/gi
+        const regex = /XMASAMXMASAMXMAS|SAMXMASAMXMASAMX|SAMXMASAMXMAS|XMASAMXMASAMX|xmasamxmas|samxmasamx|samxmas|xmasamx|xmas|samx/gi
         const c = str.match(regex)?.length || 0;
         const checkForDoubleXmas = str.match(regex);
         if (c !== 0) {
-            if (checkForDoubleXmas.includes("SAMXMAS") || checkForDoubleXmas.includes("XMASAMX")) {
-                this.count += 1; // ADDITIONAL COUNT FOR DOUBLES
-            }
+            // if (checkForDoubleXmas.includes("SAMXMASAMXMASAMX")) {
+            //     const checkHowMany = checkForDoubleXmas.some(arr => arr.includes("SAMXMASAMXMASAMX"));
+            //     this.count += 5
+            // } else if (checkForDoubleXmas.includes("XMASAMXMASAMX") || checkForDoubleXmas.includes("SAMXMASAMXMAS")) {
+            //     const checkHowMany = checkForDoubleXmas.some(arr => arr.includes("XMASAMXMASAMX"));
+            //     this.count += 4
+            // } else if (checkForDoubleXmas.includes("XMASAMXMAS") || checkForDoubleXmas.includes("SAMXMASAMX")) {
+            //     this.count += 3
+            // } else if (checkForDoubleXmas.includes("SAMXMAS") || checkForDoubleXmas.includes("XMASAMX")) {
+            //     this.count += 2
+            // } else if (checkForDoubleXmas.includes("XMAS") || checkForDoubleXmas.includes("SAMX")) {
+            //     const checkHowMany = checkForDoubleXmas.some(arr => arr.includes("XMAS"));
+            //     console.log(typeof checkHowMany)
+            //         this.count += 1; // ADDITIONAL COUNT FOR DOUBLES
+            // }
+            const totals = this.countRegexArr(checkForDoubleXmas);
+            const arrNums = Object.values(totals);
+            function sum(a,b) { return a+b; }
+            let total = 0;
+            arrNums.forEach(i => total += i);
+            this.count += total;
         }
-        this.count += c;
+        // this.count += c;
     }
     doubleLoop(el, aux) {
         for (let x = 0; x < this.width; x++) {
@@ -89,10 +108,36 @@ class CrossWord {
     }
     getWidthHeight() {
         console.log(`Width: ${this.width}, Height: ${this.height}`);
+        return this;
     }
     getCount() {
         console.log("Count:\t", this.count);
         // 2468 too low...
+        // 2476 too low...
+        // 2478 too low...
+        // 2483 wrong answer
+        // 2514 - correct!
+    }
+    countRegexArr(regArr) {
+        let obj = {};
+        for (let i = 0; i < regArr.length; i++) {
+            const element = regArr[i];
+            if (!obj[element]) {
+                obj[element] = 0;
+            }
+            if (element === "SAMXMASAMXMASAMX") {
+                obj[element] += 5
+            } else if (element === "XMASAMXMASAMX" || element === "SAMXMASAMXMAS") {
+                obj[element] += 4
+            } else if (element === "XMASAMXMAS" || element === "SAMXMASAMX") {
+                obj[element] += 3
+            } else if (element === "SAMXMAS" || element === "XMASAMX") {
+                obj[element] += 2
+            } else if (element === "XMAS" || element === "SAMX") {
+                obj[element] += 1
+            }
+        }
+        return obj;
     }
 }
 const crossword = new CrossWord(splitInput, width, height);
@@ -100,6 +145,7 @@ const crossword = new CrossWord(splitInput, width, height);
 crossword
     .processHorizontal()
     .processVertical()
+    .getWidthHeight()
     .getCount();
 
 
